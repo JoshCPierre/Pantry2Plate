88f00126f883b33b2bacde2666c75a71f03f2a73
klarissatey <klarissa.tey@gmail.com>
2026-04-08 13:45:43 -0500

    login user start

--- app/src/main/java/com/example/pantry2plate/MainActivity.kt
+++ app/src/main/java/com/example/pantry2plate/MainActivity.kt
@@ -1,47 +1,25 @@
 package com.example.pantry2plate
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pantry2plate.ui.theme.Pantry2PlateTheme
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pantry2plate.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Pantry2PlateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Pantry2PlateTheme {
-        Greeting("Android")
+        binding.registerButton.setOnClickListener {
+            // later: go to RegisterActivity
+        }
     }
 }
